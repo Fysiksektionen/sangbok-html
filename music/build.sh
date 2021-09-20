1e5cb7f1bc38894ed38609e1827553db9ef9e90a
Oskar <3982323+oskarr@users.noreply.github.com>
2021-09-20 09:56:47 +0200

    Improved convert.sh

--- music/build.sh
+++
@@ -1,9 +0,0 @@
-rm svg/*.svg
-
-./convert.sh
-./compress.sh
-
-mkdir -p ../public/msvg/
-
-cp svgs.json ../src/assets/msvgs.json
-cp svg/* ../public/msvg/
--- music/compress.sh
+++
@@ -1,8 +0,0 @@
-cd svg
-for file in *.svg
-do
-    echo -e "\e[35mKomprimerar\e[0m $file."
-    svgo -i "$file" || echo -e "\e[31mKomprimering misslyckades\e[0m för $file."
-    #scour -i "$file" -o "${file%.*}.min.svg" || echo -e "\e[31mKomprimering misslyckades\e[0m för $file."
-done
-cd ..
