1b7f73974e66a09ef52c4f7be646fab2096fa0e8
Oskar Rune <3982323+oskarr@users.noreply.github.com>
2022-06-03 21:48:30 +0000

    Fixed a JSX zoom bug.

--- src/views/song/SheetMusicRenderer.tsx
+++ src/views/song/SheetMusicRenderer.tsx
@@ -1,5 +1,6 @@
 import './SheetMusicRenderer.scss'
 import svglist from '@/assets/msvgs.json'
+import { defineComponent } from 'vue'
 
 function getZoomLevelFromFileName(filename: string) {
   return (filename.match(/-sf(\d(\.\d+)?)-/i) || ['', ''])[1]
@@ -25,39 +26,46 @@ function getImagesForZoomLevel(source: string, zoomIdx: number): string[] {
 }
 
 /** Component that renders sheet music. */
-export default function SheetMusicRenderer({ src }: { src: string }): JSX.Element {
-  // The dot needs to be here, otherwise, songs like ζ1 may think it has ζ11 as sheet music.
-  src += (src.endsWith('.') ? '' : '.')
-
-  let zoomIdx = Math.min(((window.matchMedia && window.matchMedia('only screen and (max-width: 760px)').matches)) ? 5 : 3, getZoomLevels(src).length - 1)
-  let isLoading = true
-
+export default defineComponent({
+  name: 'SheetMusicRenderer',
+  props: { src: String },
+  data() {
+    // The dot needs to be here, otherwise, songs like ζ1 may think it has ζ11 as sheet music.
+    const src2 = this.src + (this.src?.endsWith('.') ? '' : '.')
+    return {
+      src2,
+      isLoading: true,
+      zoomIdx: Math.min(((window.matchMedia && window.matchMedia('only screen and (max-width: 760px)').matches)) ? 5 : 3, getZoomLevels(src2).length - 1)
+    }
+  },
+  methods: {
   /**
    * Alters the zoom index by `z`. Generally, a positive `z` will increase the zoom by `z` levels, and decrease if `z` is negative.
    * @param z - the zoom index offset.
    */
-  function zoom(z: number) {
-    zoomIdx += z
-    zoomIdx = Math.max(0, Math.min(zoomIdx, getZoomLevels(src).length - 1))
-  }
+    zoom(z: number) {
+      this.zoomIdx += z
+      this.zoomIdx = Math.max(0, Math.min(this.zoomIdx, getZoomLevels(this.src2).length - 1))
+    }
+  },
+  render() {
+    return (
+      <div class="component-sheet-music-renderer">
+        {/* Zoom control buttons */}
+        <div class="zoombuttoncontainer">
+          <button onClick={() => this.zoom(-1)} class={{ button: true, disabled: this.zoomIdx === 0 }}>&#128269;-</button>
+          <button onClick={() => this.zoom(1)}
+            class={{ button: true, disabled: this.zoomIdx === getZoomLevels(this.src2).length - 1 }}>&#128269;+</button>
+        </div>
 
-  return (
-    <div class="component-sheet-music-renderer">
-      {/* Zoom control buttons */}
-      <div class="zoombuttoncontainer">
-        <button onClick={() => zoom(-1)} class={{ button: true, disabled: zoomIdx === 0 }}>&#128269;-</button>
-        <button onClick={() => zoom(1)}
-          class={{ button: true, disabled: zoomIdx === getZoomLevels(src).length - 1 }}>&#128269;+</button>
-      </div>
+        {/* The svg images containing sheet music. Usually there will be only one (we had more previously). */}
+        {getImagesForZoomLevel(this.src2, this.zoomIdx).map((img) =>
+          <div>
+            <img src={img} alt={this.isLoading ? 'Laddar...' : 'Noter'} onLoad={() => { this.isLoading = false }} />
+          </div>)}
 
-      {/* The svg images containing sheet music. Usually there will be only one (we had more previously). */}
-      {getImagesForZoomLevel(src, zoomIdx).map((img) =>
-        <div>
-          <img src={img} alt={isLoading ? 'Laddar...' : 'Noter'} onLoad={() => { isLoading = false }} />
-        </div>)}
-
-      {/* Error messages */}
-      {getImagesForZoomLevel(src, zoomIdx).length === 0 &&
+        {/* Error messages */}
+        {getImagesForZoomLevel(this.src2, this.zoomIdx).length === 0 &&
         <div>
           <h2>Fel</h2>
           <p style="text-align: center;">
@@ -65,13 +73,14 @@ export default function SheetMusicRenderer({ src }: { src: string }): JSX.Elemen
           </p>
         </div>}
 
-      {/* Loading message */}
-      {isLoading &&
+        {/* Loading message */}
+        {this.isLoading &&
         <div>
           <h2>Laddar...</h2>
         </div>}
 
-      <p class="notice">Notvisaren är experimentell.</p>
-    </div>
-  )
-}
+        <p class="notice">Notvisaren är experimentell.</p>
+      </div>
+    )
+  }
+})
