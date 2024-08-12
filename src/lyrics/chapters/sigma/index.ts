e5cace67c5cf950f6976aedf740a0e71ed55ea73
cousback <159156794+cousback@users.noreply.github.com>
2024-08-12 22:02:43 +0200

    Update index.ts

--- src/lyrics/chapters/sigma/index.ts
+++ src/lyrics/chapters/sigma/index.ts
@@ -3,10 +3,11 @@ import s19 from './19.json'
 import s20 from './20.json'
 import s22 from './22.json'
 import s23 from './23.json'
+import s24 from './24.json'
 // Add imports for each year here, and reference the import in the songs array below.
 
 export const sigma = {
   chapter: 'Visor vi minns',
   prefix: 'Σσ',
-  songs: [...s23, ...s22, ...s20, ...s19, ...s18]
+  songs: [...s24, ...s23, ...s22, ...s20, ...s19, ...s18]
 }
