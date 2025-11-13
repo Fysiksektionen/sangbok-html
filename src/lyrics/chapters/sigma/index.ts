1a724d2bea9abe3bd8e79b53ebb6f47a2738ab8c
Leo Trolin <leo.trolin@gmail.com>
2025-11-13 16:36:31 +0100

    Add 21.json in sigma chapter

--- src/lyrics/chapters/sigma/index.ts
+++ src/lyrics/chapters/sigma/index.ts
@@ -1,6 +1,7 @@
 import s18 from './18.json'
 import s19 from './19.json'
 import s20 from './20.json'
+import s21 from './20.json'
 import s22 from './22.json'
 import s23 from './23.json'
 import s24 from './24.json'
@@ -9,5 +10,5 @@ import s24 from './24.json'
 export const sigma = {
   chapter: 'Visor vi minns',
   prefix: 'Σσ',
-  songs: [...s24, ...s23, ...s22, ...s20, ...s19, ...s18]
+  songs: [...s24, ...s23, ...s22, ...s21, ...s20, ...s19, ...s18]
 }
